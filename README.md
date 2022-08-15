5de001d97d06b4a851a8dc0fe94a68aeca76b9e5
Jake Hughey <jakejhughey@gmail.com>
2022-08-15 14:50:43 -0500

    updated to use cases from surveycto instead of excel files.

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # ConnectEd Monitoring
 
-## Do these things once
+## To use with GitHub Actions
+
+1. Create a repository secret called "SCTO_AUTH" containing the contents of "scto_auth.txt".
+
+1. Create a repository secret called "GOOGLE_TOKEN" containing the JSON of a Google service account token.
+
+1. In the relevant Google Sheet, give editor permissions to the email address of the corresponding Google service account (e.g., xx@yy.iam.gserviceaccount.com).
+
+## To use locally
+
+### Do these things once
 
 1. Open an R session and install the necessary packages.
 
@@ -11,26 +21,12 @@
 
 1. Create a text file called "scto_auth.txt" in the params folder and add the SurveyCTO server name on the first line, username on the second line, and password on the third line.
 
-1. Open a Terminal session in the main connected_monitoring folder and run the script to process the call assignments.
-
-    ```sh
-    Rscript code/get_call_assignments.R
-    ```
-
-## Do these things each week
+### Do these things periodically
 
 1. Open a Terminal session in the main connected_monitoring folder and run the monitoring script.
 
     ```sh
-    Rscript code/get_monitoring.R
+    Rscript code/monitor.R
     ```
 
 1. Examine the resulting files in the output folder.
-
-## To use with GitHub Actions
-
-1. Create a repository secret called "SCTO_AUTH" containing the contents of "scto_auth.txt".
-
-1. Create a repository secret called "GOOGLE_TOKEN" containing the JSON of a Google service account token.
-
-1. In the relevant Google Sheet, give editor permissions to the email address of the corresponding Google service account (e.g., xx@yy.iam.gserviceaccount.com).
--- code/monitor.R
+++ code/monitor.R
@@ -5,13 +5,13 @@ library('rsurveycto')
 library('yaml')
 
 paramsDir = 'params'
-dataDir = 'data'
+# dataDir = 'data'
 outputDir = 'output'
 
 if (!dir.exists(outputDir)) dir.create(outputDir)
 
 params = read_yaml(file.path(paramsDir, 'params.yml'))
-groups = fread(file.path(paramsDir, 'biweekly_groups.csv'))
+# groups = fread(file.path(paramsDir, 'biweekly_groups.csv'))
 
 ########################################
 
@@ -41,28 +41,34 @@ if (Sys.getenv('GOOGLE_TOKEN') == '') {
 
 ########################################
 
-assignments = fread(file.path(outputDir, 'call_assignments.csv'))
-assignments[, current_phone_num_best := as.character(current_phone_num_best)]
+cases = scto_pull(auth, 'cases', cache_dir = outputDir)
+setnames(cases, c('facilitator', 'f_id'), c('facilitator_name', 'facilitator_id'))
+# cases[, current_phone_num_best := as.character(current_phone_num_best)]
+
+# assignments = fread(file.path(outputDir, 'call_assignments.csv'))
+# assignments[, current_phone_num_best := as.character(current_phone_num_best)]
 
 ########################################
 
 # TODO: convert numeric values to meanings based on
 # import_tarl_accountability_survey_r7.do
 
-acct_survey = scto_pull(auth, params$dataset_id, 'form', cache_dir = dataDir) # refresh = TRUE
+acct_survey = scto_pull(auth, params$dataset_id, 'form', cache_dir = outputDir)
 # acct_survey[, week := as.integer(week)]
 
 acct_survey[, comp_date := as.IDate(CompletionDate, format = '%b %e, %Y %I:%M:%S %p')]
 acct_survey[, week := temp_week_name]
+
 week_now = acct_survey[comp_date == max(comp_date)]$week[1L]
+acct_survey = acct_survey[week == week_now]
 
 ########################################
 
 # if (week_now == 0) week_now = max(acct_survey$week)
-group_now = groups[week == week_now]$biweekly_group
+# group_now = groups[week == week_now]$biweekly_group
 
-assignments = assignments[biweekly_group == group_now]
-acct_survey = acct_survey[week == week_now]
+# assignments = assignments[biweekly_group == group_now]
+# acct_survey = acct_survey[week == week_now]
 
 ########################################
 
@@ -79,7 +85,8 @@ fwrite(acct_dups, file.path(outputDir, 'duplicates.csv'))
 by_cols = c('facilitator_name', 'facilitator_id', 'id')
 
 acct_monitor = merge(
-  assignments[, ..by_cols],
+  # assignments[, ..by_cols],
+  cases[, ..by_cols],
   unique(acct_survey[, ..by_cols])[, submitted := 1L],
   by = by_cols, all.x = TRUE)
 
@@ -98,8 +105,8 @@ fwrite(acct_summary, file.path(outputDir, 'summary.csv'))
 ########################################
 
 acct_missing = merge(
-  acct_monitor[submitted == 0, !'submitted'],
-  assignments[, !'biweekly_group'], by = by_cols)
+  acct_monitor[submitted == 0, !'submitted'], cases, by = by_cols)
+  # assignments[, !'biweekly_group'], by = by_cols)
 
 write_sheet(acct_missing, params$googlesheet_id, 'missing')
 fwrite(acct_missing, file.path(outputDir, 'missing.csv'))
@@ -108,7 +115,6 @@ fwrite(acct_missing, file.path(outputDir, 'missing.csv'))
 
 status = data.table(
   week = week_now,
-  biweekly_group = group_now,
   last_updated_utc = Sys.time())
 
 write_sheet(status, params$googlesheet_id, 'status')
